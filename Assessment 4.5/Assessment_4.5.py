d25f54997254ece0f5ff1c64a93f5bcf60e30b05
white.witt192@gmail.com <white.witt192@gmail.com>
2024-11-05 13:55:48 +1300

    Changed the titles of the table rows

--- Assessment 4.5/Assessment_4.5.py
+++ Assessment 4.5/Assessment_4.5.py
@@ -13,7 +13,7 @@ dget1.title("Assessment 4 Main")
 
 allRecords = Button(dget1, text="All Records", command=BackEnd.allRec).grid(row=5, column=1, padx=10, pady=5)
 posGrowth = Button(dget1, text="Positive Growth", command=BackEnd.posGrowth).grid(row=5, column=3, padx = 10, pady=5)
-queryBtn = Button(dget1, text="Query Record by Date".grid(row=5, column=4, padx = 10, pady = 5)
-countBtn = Button(dget1, text="Count Companies Between Date ".grid(row=10, column=3, padx = 10, pady=5)
+queryBtn = Button(dget1, text="Query Record by Date").grid(row=5, column=4, padx = 10, pady = 5)
+countBtn = Button(dget1, text="Count Companies Between Date").grid(row=10, column=3, padx = 10, pady=5)
 
 dget1.mainloop()
--- Assessment 4/Assessment_4.py
+++ Assessment 4/Assessment_4.py
@@ -71,7 +71,7 @@ class BackEnd():
             cursor.close()
             conn.close()
         
-def     query():
+    def query():
         db_file = r'C:\Users\2024001678\Downloads\Assessment 4.accdb'
 
         # Establish a connection to the database
@@ -106,7 +106,7 @@ def     query():
             cursor.close()
             conn.close()
 
-def count():
+    def count():
         db_file = r'C:\Users\2024001678\Downloads\Assessment 4.accdb'
 
         # Establish a connection to the database
@@ -127,12 +127,17 @@ def count():
 
             # Print column headers
             columns = [column[0] for column in cursor.description]
-            print('\t'.join(columns))
+            print(f'{"Company_name":<30}{"Industry":<30}{"Year_revenue":<20}{"Revenue_growth":<20}{"Number_of_employees":<30}{"Headquarters":<20}{"Company_found_date":<20}')
 
             # Print each row
             for row in rows:
-                print('\t'.join(str(item) for item in row))
+                # print('\t'.join(str(item) for item in row))
 
+                CoFoundDate = row.Company_found_date.strftime("%d-%m-%Y")
+                
+                List_company = f'{row.Company_name:<30}{row.Industry:<30}{row.Year_revenue:<20}{row.Revenue_growth:<20}{row.Number_of_employees:<30}{row.Headquarters:<20}{CoFoundDate:<20}'
+                print(List_company)
+               
         except pyodbc.Error as e:
             print(f"Error accessing database: {e}")
 
@@ -140,88 +145,3 @@ def count():
             # Close cursor and connection
             cursor.close()
             conn.close()
-
-from tkinter import *
-import pyodbc
-
-# back end
-
-def showBtn():
-    db_file = r'C:\Users\ongchangcoj\Downloads\AccessConnect2_demoDB.accdb'
-
-    # Establish a connection to the database
-    conn_str = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=' + db_file
-    conn = pyodbc.connect(conn_str)
-
-    # Create a cursor object to interact with the database
-    cursor = conn.cursor()
-
-    sqlComm = 'SELECT * FROM People;'
-
-    try:
-        # Execute a SELECT query
-        cursor.execute(sqlComm)  # Replace YourTableName with your table name
-
-        # Fetch all rows from the result set
-        rows = cursor.fetchall()
-
-        # Print column headers
-        columns = [column[0] for column in cursor.description]
-        print('\t'.join(columns))
-
-        # Print each row
-        for row in rows:
-            print('\t'.join(str(item) for item in row))
-
-    except pyodbc.Error as e:
-        print(f"Error accessing database: {e}")
-
-    finally:
-        # Close cursor and connection
-        cursor.close()
-        conn.close()
-
-
-# front end
-
-widget = Tk() # creates the widget object from tkinter
-
-widget.title("Assessment 2")
-widget.geometry("400x200")
-
-# creating labels and textboxes
-
-titleLabel = Label(widget, text="Title")
-MratingLabel = Label(widget, text="Maturity rating")
-RdateLabel = Label(widget, text="Release date")
-reviewLabel = Label(widget, text="Review")
-
-titleTxtbx = Entry(widget, width=10)
-MratingTxtbx = Entry(widget, width=10)
-RdateTxtbx = Entry(widget, width=10)
-reviewTxtbx = Entry(widget, width=10)
-
-# first column elements
-titleLabel.grid(row=1, column=1)
-RdateLabel.grid(row=4, column=1)
-
-# 2nd column elements
-titleTxtbx.grid(row=1, column=2, padx=10, pady=5)
-RdateTxtbx.grid(row=4, column=2, padx=10, pady=5)
-
-# 3rd column elements
-MratingLabel.grid(row=1, column=3, padx=10, pady=5)
-reviewLabel.grid(row=4, column=3, padx=10, pady=5)
-
-# 4th column elements
-MratingTxtbx.grid(row=1, column=4, padx=10, pady=5)
-reviewTxtbx.grid(row=4, column=4, padx=10, pady=5)
-
-# create buttons
-
-submitBtn = Button(widget, text="Submit").grid(row=7, column=2, padx = 10, pady=5)
-showBtn = Button(widget, text="Show all", command=showBtn).grid(row=7, column=3, padx = 10, pady=5)
-quitBtn = Button(widget, text="Quit").grid(row=7, column=4, padx = 10, pady=5)
-
-widget.mainloop() # runs mainloop to show widget
-
